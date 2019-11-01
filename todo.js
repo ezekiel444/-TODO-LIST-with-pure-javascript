6969b5b0273ae1d7c1500fd7a73426ba6e14699f
ezekiel444 <ezekielmatomi@gmail.com>
2019-11-01 17:27:38 +0100

    Simple Todo-List with pure javascript and html5.

---
+++ todo.js
@@ -0,0 +1,20 @@
+let formfield = document.getElementById('formfield')
+let textfield = document.getElementById('textfield')
+let mylist = document.getElementById('mylist')
+
+
+formfield.addEventListener("submit",(e)=>{
+e.preventDefault();
+createItem(textfield.value)
+})
+
+function createItem(x) {
+  let myHtml =   `<li>${x}<button onClick='buttonDelete(this)'>Delete</button></li>`
+  mylist.insertAdjacentHTML('beforeend',myHtml)
+  textfield.value = ''
+  textfield.focus()
+}
+
+function buttonDelete(toDelete){
+toDelete.parentElement.remove()
+}
